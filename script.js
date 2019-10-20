7f70811d92cfed7056fa84229fa8d150de349373
parth-2705 <40501175+parth-2705@users.noreply.github.com>
2019-10-21 01:31:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
- // portfolio
+  // portfolio
  $('.gallery ul li a').click(function() {
      var itemID = $(this).attr('href');
      $('.gallery ul').addClass('item_open');
      $(itemID).addClass('item_open');
+     $('body').addClass('noscroll');
+	 $('.item_open').addClass('scroll');
      return false;
  });
  $('.close').click(function() {
      $('.port, .gallery ul').removeClass('item_open');
+	 $('body').removeClass('noscroll');
+	 $('.item_open').removeClass('scroll');
      return false;
  });
 
